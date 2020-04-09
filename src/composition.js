025e1e944b02492c36c72f8288694cf91f3b27b9
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-04-09 23:41:44 +0200

    playing with composition

---
+++ src/composition.js
@@ -0,0 +1,36 @@
+const { compose, prop, last, map } = require('ramda');
+const assert = require('assert').strict;
+
+// normalize usernames
+const toLower = (str) => str.toLowerCase();
+const underscore = (str) => str.replace(/\s/g, '_');
+const normalize = compose(toLower, underscore);
+
+assert.equal(normalize('Evandro Goncalves'), 'evandro_goncalves');
+
+// last item in stock
+const items = [
+  { name: 'Nintendo Switch', in_stock: true },
+  { name: 'Playstation 4', in_stock: true },
+  { name: 'Xbox', in_stock: false },
+  { name: 'LG TV', in_stock: false },
+  { name: 'Samsung TV', in_stock: true },
+];
+
+const isLastInStock = compose(prop('in_stock'), last);
+assert.ok(isLastInStock(items));
+
+// name last item
+const nameLastItem = compose(prop('name'), last);
+assert.equal(nameLastItem(items), 'Samsung TV');
+
+// normalize item names
+const normalizeItemNames = map(compose(toLower, underscore, prop('name')));
+
+assert.deepEqual(normalizeItemNames(items), [
+  'nintendo_switch',
+  'playstation_4',
+  'xbox',
+  'lg_tv',
+  'samsung_tv',
+]);
