ef228c8e00afb548da2870a3f82937cd8240b714
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-04-13 20:06:59 +0200

    fix more problem with either monad

--- src/eitherMonad.js
+++ src/eitherMonad.js
@@ -1,14 +1,26 @@
 const assert = require('assert').strict;
 
 const Right = (value) => ({
+    chain: (f) => f(value),
     map: (f) => Right(f(value)),
     fold: (f, g) => g(value),
 });
 
 const Left = (value) => ({
+    chain: (f) => Left(value),
     map: (f) => Left(f(value)),
     fold: (f, g) => f(value),
 });
+
+const fromNullable = (value) => (value ? Right(value) : Left());
+
+const tryCatch = (callback) => {
+    try {
+        Right(callback());
+    } catch (e) {
+        Left(e);
+    }
+};
 
 // find color
 const findColor = (key) => {
@@ -30,3 +42,14 @@ const color = (value) =>
 
 assert.equal(color('red'), '#FF4444');
 assert.equal(color('redd'), 'no color!');
+
+// street name
+const street = (user) =>
+    fromNullable(user.address).fold(
+        () => 'no street!',
+        (address) => address.street,
+    );
+
+const user = { address: { street: { name: 'Criciuma' } } };
+assert.deepEqual(street(user), { name: 'Criciuma' });
+assert.equal(street({}), 'no street!');
