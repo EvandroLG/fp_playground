a9bde9d95fc174a9b6604432711048509531de60
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-03-20 22:58:23 +0100

    implement recursive map

--- src/recursion.js
+++ src/recursion.js
@@ -1,13 +1,23 @@
 const assert = require('assert');
 
 // sum
-const sum = (arr) => (
-  arr.length === 1 ? arr[0] : arr[0] + sum(arr.slice(1))
-);
-
+const sum = (arr) => (arr.length === 1 ? arr[0] : arr[0] + sum(arr.slice(1)));
 assert.equal(sum([1, 2, 3, 4]), 10);
 
 // fibonacci
 const fibonacci = (n) => (n === 1 ? 1 : n * fibonacci(n - 1));
-
 assert.equal(fibonacci(3), 6);
+
+// map
+const map = (resolver, arr) => {
+    if (!arr.length) {
+        return [];
+    }
+
+    return [resolver(arr[0])].concat(map(resolver, arr.slice(1)));
+};
+
+assert.deepEqual(
+    map((item) => item + 2, [1, 2, 3, 4]),
+    [3, 4, 5, 6],
+);
